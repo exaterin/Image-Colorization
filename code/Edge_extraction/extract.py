3c531923a25124d339966803b52777334cd1faa2
Lipina Ekaterina <983-lipinae@users.noreply.localhost>
2023-11-11 17:21:13 +0100

    Code for three edge extractors

---
+++ code/Edge_extraction/extract.py
@@ -0,0 +1,102 @@
+import os
+import glob
+import numpy as np
+import argparse
+import cv2 as cv
+from skimage.io import imsave
+from skimage.color import rgb2gray
+from cv2 import GaussianBlur, imread
+import matplotlib.pyplot as plt
+
+# Constants for XDoG line extraction
+GAMMA = 0.95
+SIGMA = 1
+K = 3
+EPSILON = -0.05
+PHI = 100
+
+# Constants for Sobel line extraction
+THRESH = 100
+
+# Constants for Canny line extraction
+THRESH1 = 100
+THRESH2 = 200
+
+def xdog(image, gamma=GAMMA, sigma=SIGMA, k=K, epsilon=EPSILON, phi=PHI):
+    if image.ndim == 3 and image.shape[2] == 3:
+        image = rgb2gray(image)
+
+    image1 = GaussianBlur(image, (0, 0), sigma)
+    image2 = GaussianBlur(image, (0, 0), sigma * k)
+
+    difference = image1 - gamma * image2
+    mask = difference < epsilon
+
+    difference[mask] = 1
+    difference[~mask] = 1 + np.tanh(phi * difference[~mask])
+
+    # Normalize the difference to the range [0, 255]
+    normalized_difference = (difference - difference.min()) / (difference.max() - difference.min())
+    return (normalized_difference * 255).astype(np.uint8)
+
+def sobel(img, thresh=THRESH):
+
+    if img.ndim == 3 and img.shape[2] == 3:
+        img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+    mx = np.array( [[ -1, 0, 1 ], [ -2, 0, 2], [ -1, 0, 1]] )
+    gx = cv.filter2D(img, ddepth=-1, kernel=mx)
+    gy = cv.filter2D(img, ddepth=-1, kernel=np.rot90(mx))  
+    edges = np.hypot(gx, gy) >= thresh   
+    return (255 - (edges.astype(np.uint8) * 255)) 
+
+
+def canny(img, thresh1=THRESH1, thresh2=THRESH2):
+    edges = cv.Canny(img, thresh1, thresh2)
+    return 255 - edges
+
+
+image_folder = 'Images'
+
+# Get the image from the dataset folder
+IMAGE_INDEX = 34
+
+image_files = glob.glob(os.path.join(image_folder, '*.jpg'))
+first_image_path = image_files[IMAGE_INDEX] if image_files else None
+
+if first_image_path:
+    # Load the first image
+    original_image = imread(first_image_path)
+    original_image = cv.cvtColor(original_image, cv.COLOR_BGR2RGB)
+
+    # Apply edge extraction methods
+    xdog_image = xdog(original_image)
+    sobel_image = sobel(original_image)
+    canny_image = canny(original_image)
+
+  # Median of three edge detectors
+    med_edges = np.median([xdog_image, sobel_image, canny_image], axis=0)
+
+    # Plotting the results
+    fig, axes = plt.subplots(1, 5, figsize=(25, 5))
+    axes[0].imshow(original_image)
+    axes[0].set_title('Original Image')
+    axes[0].axis('off')
+
+    axes[1].imshow(xdog_image, cmap='gray')
+    axes[1].set_title('XDoG Edge Detection')
+    axes[1].axis('off')
+
+    axes[2].imshow(sobel_image, cmap='gray')
+    axes[2].set_title('Sobel Edge Detection')
+    axes[2].axis('off')
+
+    axes[3].imshow(canny_image, cmap='gray')
+    axes[3].set_title('Canny Edge Detection')
+    axes[3].axis('off')
+
+    axes[4].imshow(med_edges, cmap='gray')
+    axes[4].set_title('Median of Edge Detectors')
+    axes[4].axis('off')
+
+    plt.show()
--- code/dataset.py
+++ code/dataset.py
@@ -18,26 +18,28 @@ class ImageDataset(Dataset):
         return len(self.image_files)
 
     def __getitem__(self, idx):
-        img_path = os.path.join(self.image_folder, self.image_files[idx])
-        image = Image.open(img_path)  # Load the image
-        image = image.convert('RGB')  # Convert to RGB format, if needed
+        image_name = self.image_files[idx]
+        image_path = os.path.join(self.image_folder, image_name)
+        image = Image.open(image_path)
 
         return image
     
     # Split the dataset into train and test sets
     @staticmethod
-    def split_dataset(image_folder, test_size=0.2, random_state=None, save_csv=False, transform=None):
+    def split_dataset(image_folder, test_size=0.2, path='Code/Splits',random_state=None, save_csv=False, transform=None):
         image_files = [f for f in os.listdir(image_folder) if os.path.isfile(os.path.join(image_folder, f))]
         train_files, test_files = train_test_split(image_files, test_size=test_size, random_state=random_state)
         if save_csv:
             combined_list = [(file, 'train') for file in train_files] + [(file, 'test') for file in test_files]
             combined_df = pd.DataFrame(combined_list, columns=['filename', 'split'])
-            combined_df.to_csv('combined_splits.csv', index=False)
+            if not os.path.exists(path):
+                os.makedirs(path)
+            combined_df.to_csv(f'{path}/combined_splits.csv', index=True)
         return ImageDataset(image_folder, train_files, transform), ImageDataset(image_folder, test_files, transform)
 
     # Create a split from a csv file
     @staticmethod
-    def create_split_from_csv(image_folder, csv_file='combined_splits.csv', transform=None):
+    def create_split_from_csv(image_folder, csv_file='Code/Splits/combined_splits.csv', transform=None):
         file_df = pd.read_csv(csv_file)
         train_files = file_df[file_df['split'] == 'train']['filename'].tolist()
         test_files = file_df[file_df['split'] == 'test']['filename'].tolist()
